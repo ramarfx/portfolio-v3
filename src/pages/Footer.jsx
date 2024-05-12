e97e09cdfbe1c22e3f1d51d5bcc53aa0564fdc5f
ramarfx <ramagamiing33@gmail.com>
2024-05-12 18:38:43 +0700

    Accesibility

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -5,16 +5,16 @@ const Footer = () => {
         <div className="flex flex-col-reverse flex-wrap items-center gap-2 p-8">
           <p className="font-poppins text-sm text-primary">2023 - Ramarfx</p>
           <div className="sosmed text-base text-primary">
-            <a href="https://discordapp.com/users/755793484392300624">
+            <a href="https://discordapp.com/users/755793484392300624" aria-label="link to my discord">
               <i className="fa-brands fa-discord"></i>
             </a>
-            <a href="https://github.com/ramarfx">
+            <a href="https://github.com/ramarfx" aria-label="link to my github">
               <i className="fa-brands fa-github"></i>
             </a>
-            <a href="https://www.instagram.com/ramtxh/">
+            <a href="https://www.instagram.com/ramtxh/" aria-label="link to my instagram">
               <i className="fa-brands fa-instagram"></i>
             </a>
-            <a href="https://facebook.com/ramadinaalmuthazam">
+            <a href="https://facebook.com/ramadinaalmuthazam" aria-label="link to my facebook">
               <i className="fa-brands fa-square-facebook"></i>
             </a>
           </div>
